d8a8552a90c5b1c9477f98d9a19620657cf33560
Zoe Ma <Zma5319@conestogac.on.ca>
2023-05-28 21:40:23 -0400

    add another item in order

--- assignment1Shwarma.js
+++ assignment1Shwarma.js
@@ -5,13 +5,18 @@ const OrderState = Object.freeze({
   MENU: Symbol("menu"),
   SIZE: Symbol("size"),
   TOPPINGS: Symbol("toppings"),
+  SPRINGROLLS: Symbol("springrolls"),
   DRINKS: Symbol("drinks"),
+  SECONDITEM: Symbol("secondItem"),
+  SECONDSIZE: Symbol("secondSize"),
+  SECONDTOPPING: Symbol("secondTopping"),
 });
 const price = {
-  small: 12,
-  medium: 14,
-  large: 18,
+  pho: { small: 12, medium: 14, large: 18 },
+  rice: { small: 10, medium: 12, large: 16 },
+  soup: { small: 8, medium: 10, large: 13 },
   toppings: 5,
+  springroll: { vegetable: 5, shrimp: 7, tofu: 5 },
   drinks: 3,
 };
 module.exports = class ShwarmaOrder extends Order {
@@ -22,6 +27,10 @@ module.exports = class ShwarmaOrder extends Order {
     this.sToppings = "";
     this.sDrinks = "";
     this.sItem = "";
+    this.springroll = "";
+    this.sSecondItem = "";
+    this.sSecondSize = "";
+    this.sSecondTopping = "";
     this.total = 0;
   }
   handleInput(sInput) {
@@ -30,7 +39,7 @@ module.exports = class ShwarmaOrder extends Order {
       case OrderState.WELCOMING:
         this.stateCur = OrderState.MENU;
         aReturn.push("Welcome to Pho Vietnam KW");
-        aReturn.push("What would you like? Pho, Fried Rice, Pad Thai?");
+        aReturn.push("What would you like? Pho, Rice, Soup?");
         break;
       case OrderState.MENU:
         this.stateCur = OrderState.SIZE;
@@ -40,14 +49,48 @@ module.exports = class ShwarmaOrder extends Order {
       case OrderState.SIZE:
         this.stateCur = OrderState.TOPPINGS;
         this.sSize = sInput;
-        this.total += price[sInput];
+        this.total += price[this.sItem][this.sSize];
         aReturn.push("What toppings would you like?Beef, Chicken, Shrimp?");
         break;
       case OrderState.TOPPINGS:
-        this.stateCur = OrderState.DRINKS;
+        this.stateCur = OrderState.SECONDITEM;
         this.sToppings = sInput;
         this.total += price.toppings;
-        aReturn.push("Would you like drinks with that?");
+        aReturn.push("Would you like another item?Pho, Rice, Soup?");
+        break;
+      case OrderState.SECONDITEM:
+        if (sInput.toLowerCase() != "no") {
+          this.sSecondItem = sInput;
+          this.stateCur = OrderState.SECONDSIZE;
+          aReturn.push("What size would you like?");
+        } else {
+          this.stateCur = OrderState.SPRINGROLLS;
+          aReturn.push(
+            "Would you like a spring roll with your order?Vegetable, shrimp, tofu?"
+          );
+        }
+        break;
+      case OrderState.SECONDSIZE:
+        this.stateCur = OrderState.SECONDTOPPING;
+        this.sSecondSize = sInput;
+        this.total += price[this.sSecondItem][this.sSecondSize];
+        aReturn.push("What toppings would you like?Beef, Chicken, Shrimp?");
+        break;
+      case OrderState.SECONDTOPPING:
+        this.stateCur = OrderState.SPRINGROLLS;
+        this.sSecondTopping = sInput;
+        this.total += price.toppings;
+        aReturn.push(
+          "Would you like a spring roll with your order?Vegetable, shrimp, tofu?"
+        );
+        break;
+      case OrderState.SPRINGROLLS:
+        this.stateCur = OrderState.DRINKS;
+        if (sInput.toLowerCase() != "no") {
+          this.springroll = sInput;
+          this.total += price.springroll[sInput];
+        }
+        aReturn.push("Would you like a drink with your order?");
         break;
       case OrderState.DRINKS:
         this.isDone(true);
@@ -57,6 +100,11 @@ module.exports = class ShwarmaOrder extends Order {
         }
         aReturn.push("Thank-you for your order of");
         aReturn.push(`${this.sSize} ${this.sItem} with ${this.sToppings}`);
+        if (this.sSecondItem) {
+          aReturn.push(
+            `${this.sSecondSize} ${this.sSecondItem} with ${this.sSecondTopping}`
+          );
+        }
         if (this.sDrinks) {
           aReturn.push(this.sDrinks);
         }
